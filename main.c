fd1bb6119aef8edaed6f49a0d886aa7d4d2895f7
bcionescu <bcionescu@users.noreply.github.com>
2026-03-22 00:06:10 +0200

    Early stage, break a string down into groups of three.

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+// Haven't touched C in a few months. Building this spellchecker will be a great way to not only refresh my knowledge, but to learn ever further.
+
 int main() {
-  char *word = "calpentry";
+  char word[] = "calpentry";
+  char *p = word;
+  int third = 0;
+
+  while (*p) {
+    printf("%c", *p);
+    p++;
+
+    if (third < 2) {
+      third++;
+    } else {
+      third = 0;
+      printf("\n");
+    }
+  }
 }
+
+
