c7b5b3b02bfe8a6b8a971a9247fe3b63a1412bf3
bcionescu <bcionescu@users.noreply.github.com>
2026-03-22 19:42:14 +0200

    The program now calculates how long it took to run.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct {
   char word[32];
@@ -331,9 +332,11 @@ int compare_matches(const void *a, const void *b) {
 
 int main() {
 
+  clock_t start = clock();
+
   int count = 0;
 
-  char misspelled[] = "occurence";
+  char misspelled[] = "calpentry";
 
   char path[16];
   int len = strlen(misspelled);
@@ -396,9 +399,39 @@ int main() {
     printf("%s [%d]\n", results[i].word, results[i].points);
   }
 
-  // To make the Nvim Nerdlist of words more efficient, we break it down into lists based on length (2 letter words, 3 letter words, etc.)
+  clock_t end = clock();
 
-  // We load the list that matches the misspelled word's length, and check. If we don't get a sufficiently high score, we expand to -1/+1 length. If we still don't get a good match, we do -2/+2, etc.
+  double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+  printf("\n\%fs", time_taken);
+
+  // This can be improved upon. Many words in the English language have pairs of 2.
+  // cc – as in accommodate, success
+  // ee – as in tree, feel
+  // ff – as in off, coffee
+  // gg – as in bigger, sagging
+  // bb – as in hobby
+  // ii – as in skiing, literature
+  // ll – as in bell, call
+  // mm – as in summer, grammar
+  // nn – as in dinner, funny
+  // oo – as in moon, book
+  // pp – as in apple, happy
+  // rr – as in mirror, terror
+  // ss – as in pass, grass
+  // tt – as in letter, better
+  // uu – as in vacuum, continuum
+  // vv – as in savvy
+
+  // In some words these pairs occur once, and in others they occur twice.
+  // Pairs that only appear once: bb, cc, ii, vv
+  // Pairs that can appear twice: ee, ff, gg, ll, mm, nn, oo, pp, rr, ss, tt, uu
+
+  // Before we do anything else, look for b, c, i, and v in the word, and double it. Does this now perfectly match a word in the dictionary? If so, print i.
+
+  // Then, double e, f, g, l, m, n, o, p, r, s, t, and u, and check them against the dictionary.
+
+  // Also add a manual list which supersedes all other processes, and shows up first (maybe).
 
   return 0;
 }
