8d53407bfa27033ce9d5f593bf705acbeca7931a
bcionescu <bcionescu@users.noreply.github.com>
2026-03-25 01:18:32 +0200

    get_max_results now returns the amount of memory to allocate for the results, instead of using an arbitrarily large number, such as 400,000 or 50,000

--- main.c
+++ main.c
@@ -2790,6 +2790,38 @@ int manual_list(char *miss, char *line) {
   return score;
   }
 
+int get_max_results(char letter) {
+
+  if (letter == 'a') return 10441;
+  if (letter == 'b') return 9275;
+  if (letter == 'c') return 16229;
+  if (letter == 'd') return 10147;
+  if (letter == 'e') return 7054;
+  if (letter == 'f') return 6766;
+  if (letter == 'g') return 5588;
+  if (letter == 'h') return 6300;
+  if (letter == 'i') return 6977;
+  if (letter == 'j') return 1375;
+  if (letter == 'k') return 1715;
+  if (letter == 'l') return 5052;
+  if (letter == 'm') return 9623;
+  if (letter == 'n') return 4389;
+  if (letter == 'o') return 5836;
+  if (letter == 'p') return 14562;
+  if (letter == 'q') return 822;
+  if (letter == 'r') return 10158;
+  if (letter == 's') return 18986;
+  if (letter == 't') return 8746;
+  if (letter == 'u') return 5068;
+  if (letter == 'v') return 2735;
+  if (letter == 'w') return 3693;
+  if (letter == 'x') return 134;
+  if (letter == 'y') return 544;
+  if (letter == 'z') return 532;
+
+  return 0;
+}
+
 int main() {
 
     char *misspelled[24] = {"auther", "teh","recieve","seperate","definately","occured","untill","wich","becuase","thier","goverment","enviroment","acommodate","arguement","calender","concious","embarass","existance","foriegn","happend","independant","lable","neccessary"};
@@ -2814,7 +2846,7 @@ int main() {
       }
 
       int len = strlen(misspelled[i]);
-      int max_results = 20000;
+      int max_results = get_max_results(misspelled[i][0]);
       int entry_count = 0;
 
       char line_buffer[30];
